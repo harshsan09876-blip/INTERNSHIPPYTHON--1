023b8eac13809cef2fe7150c7b6910e909ebe347
githubpractice <harshsan09876@gmail.com>
2026-08-24 14:45:49 +0530

    we have a successful python file for first day internship

---
+++ personal_information.py
@@ -0,0 +1,30 @@
+# PERSONAL INFORMATION PROFILE
+# this ia a structur code to think 
+
+
+#name = str(input("Enter your name : "))
+
+#course = str(input("Enter ypur name:"))
+#age = int(input("Enter your class: "))
+#city = str(input("Enetr your class: "))
+#college = str(input("Enter your name:"))
+#grade = char(input("Enter your grade: "))
+#cgpa = char(input("Enter your name: "))
+
+name = str(input("Enter your name: "))
+course = str(input("Enter your course:"))
+college = str(input("Enter your college name: "))
+city = str(input("Enter your city name: "))
+grade = str(input("Enter your grade: "))
+cgpa = float(input("Enter your cgpa: "))
+age = int(input("Enter your age: "))
+
+
+print(f"\n--student details--")
+print(f"Name: {name}")
+print(f"course:{course}")
+print(f"cgpa:{cgpa}")
+print(f"city:{city}")
+print(f"college:{college}")
+print(f"age:{age}")
+print(f"grade:{grade}")
